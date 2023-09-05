bf9d24facd9784fc6bd61b5efc38354a69551be0
Apple <apple@macbookpro.local>
2023-09-05 16:33:17 +0700

    fire events for request review Android

--- android/src/main/java/com/oblador/storereview/StoreReviewModuleImpl.java
+++ android/src/main/java/com/oblador/storereview/StoreReviewModuleImpl.java
@@ -1,14 +1,21 @@
 package com.oblador.storereview;
 
+import android.os.Parcel;
 import android.util.Log;
 import androidx.annotation.NonNull;
 import java.util.Map;
 import java.util.HashMap;
+
+import com.google.android.play.core.review.ReviewException;
 import com.google.android.play.core.review.ReviewManager;
 import com.google.android.play.core.review.ReviewManagerFactory;
 import com.google.android.play.core.review.ReviewInfo;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.Arguments;
 import com.google.android.gms.tasks.Task;
+import com.google.android.play.core.review.model.ReviewErrorCode;
 
 public class StoreReviewModuleImpl {
 
@@ -18,12 +25,19 @@ public class StoreReviewModuleImpl {
         ReviewManager manager = ReviewManagerFactory.create(context);
         Task<ReviewInfo> request = manager.requestReviewFlow();
         request.addOnCompleteListener(task -> {
+            WritableMap params = Arguments.createMap();
             if (task.isSuccessful()) {
                 ReviewInfo reviewInfo = task.getResult();
                 manager.launchReviewFlow(context.getCurrentActivity(), reviewInfo);
+                params.putBoolean("success", true);
+                params.putString("reviewInfo", reviewInfo.toString());
             } else {
-                Log.w(NAME, "Requesting review failed");
+                String errorMsg = task.getException().getMessage();
+                params.putBoolean("success", false);
+                params.putString("error", errorMsg);
             }
+            context.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+              .emit("EmitStoreReviewResult", params);
         });
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,45 @@
-import RNStoreReview from "./NativeRNStoreReview"
-
+import RNStoreReview from './NativeRNStoreReview';
+import { NativeEventEmitter, NativeModules } from 'react-native';
 /**
  * Asks the user to rate the app in the App/Play Store.
  * @throws Will throw an Error if native module is not present or not supported by the OS version.
  */
-export function requestReview() {
+
+let currentListener: NativeEventEmitter | undefined = undefined;
+let eventEmiter = undefined;
+export const requestReview = () => {
   if (!RNStoreReview) {
-    throw new Error('StoreReview native module not available, did you forget to link the library?');
+    throw new Error(
+      'StoreReview native module not available, did you forget to link the library?'
+    );
   }
 
   return RNStoreReview.requestReview();
-}
+};
+
+export type RequestReviewResult = {
+  success: boolean;
+  error?: string;
+  reviewInfo?: string;
+};
+
+export const addRequestReviewListener = (
+  listener: (RequestReviewResult) => void
+) => {
+  if (!RNStoreReview) {
+    throw new Error(
+      'StoreReview native module not available, did you forget to link the library?'
+    );
+  }
+
+  if (!eventEmiter) {
+    eventEmiter = new NativeEventEmitter(NativeModules.RNStoreReview);
+  }
+  
+  if (!!currentListener) {
+    currentListener.remove();
+    currentListener = undefined;
+  }
+
+  currentListener = eventEmiter.addListener('EmitStoreReviewResult', listener);
+};
